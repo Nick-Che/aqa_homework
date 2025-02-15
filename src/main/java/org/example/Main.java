a30d4a343afc35aba03d768a0be39aeebcf0cab2
Nickolai Chernyshev <nickchern2013@gmail.com>
2025-02-15 18:13:57 +0300

    Дз 10

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,52 +1,42 @@
 package org.example;
 
-import java.util.*;
-
-import static org.example.Cat.getCounter;
-
 public class Main {
     public static void main(String[] args) {
-        Animal bobbik = new Dog("Боббик");
-        Dog sharik = new Dog("Шарик");
-        Dog hanchik = new Dog("Ханчик");
+        String[][] matrix = {{"1", "2", "1", "7"},
+                {"9", "1", "5", "4"},
+                {"6", "0", "1", "1"},
+                {"7", "2", "1", "1"}};
 
-        Bowl bowl = new Bowl(20);
-
-        bowl.fill(30);
-
-        Cat[] cats = new Cat[4];
-        cats[0] = new Cat("Барсик");
-        cats[1] = new Cat("Персик");
-        cats[2] = new Cat("Олег");
-        cats[3] = new Cat("Китти");
-
-        for (int i = 0; i < 4; i++) {
-            cats[i].eat(15, bowl);
+        try {
+            int sum = processMatrix(matrix);
+            System.out.println("Сумма елементов матрицы: " + sum);
+        } catch (MyArraySizeException | MyArrayDataException e) {
+            System.out.println(e.getMessage());
         }
-
-        System.out.println(Animal.getCounter());
-        System.out.println(Dog.getCounter());
-        System.out.println(Cat.getCounter());
-
-        sharik.run(700);
-        hanchik.swim(7);
-        cats[2].run(150);
-        cats[0].swim(31);
-
-        Shape circle = new Circle(5, "Красный", "Черный");
-        Shape rectangle = new Rectangle(4, 6, "Зеленый", "Желтый");
-        Shape triangle = new Triangle(3, 4, 5, "Синий", "Розовый");
-
-        printShapeDetails(circle);
-        printShapeDetails(rectangle);
-        printShapeDetails(triangle);
     }
 
-    public static void printShapeDetails(Shape shape) {
-        System.out.println("Периметр: " + shape.calculatePerimeter());
-        System.out.println("Площадь: " + shape.calculateArea());
-        System.out.println("Цвет фона: " + shape.getFillColor());
-        System.out.println("Цвет границ: " + shape.getBorderColor());
-        System.out.println();
+    public static int processMatrix(String[][] matrix) throws MyArraySizeException, MyArrayDataException {
+        if (matrix.length == 4) {
+            for (int i = 0; i < 4; i++) {
+                if (matrix[i].length != 4) {
+                    throw new MyArraySizeException("Неверный размер матрицы. Ожидаемый размер 4х4.");
+                }
+            }
+        } else {
+            throw new MyArraySizeException("Неверный размер матрицы. Ожидаемый размер 4х4.");
+        }
+
+        int sum = 0;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                try {
+                    sum += Integer.parseInt(matrix[i][j]);
+                } catch (NumberFormatException e) {
+                    throw new MyArrayDataException("Неверный формат данных в ячейке (" + i + ", " + j + "): " + matrix[i][j]);
+                }
+            }
+        }
+
+        return sum;
     }
 }
